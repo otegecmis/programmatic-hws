7453ab8d7918f754695e01b659c044d560470344
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-08-22 11:47:43 +0300

    chore: create a `project` (#1)

---
+++ ocean-viewer/Controller/HomeController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class HomeController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
